41707f50e8f1ea637d32b726be97e2840ecc2630
sic <97604314+Simone-Tuzza@users.noreply.github.com>
2022-03-24 12:12:07 +0100

    Aggiunta elementi per sottoprogrammi
    Aggiunti elementi per calcolo sha e la verifica della firma compresi anche gli elementi di output

--- 03-codice/ValidaDownloadByPeakyBlinders/ValidaDownloadByPeakyBlinders/obj/Debug/net5.0-windows/App.g.i.cs
+++ 03-codice/ValidaDownloadByPeakyBlinders/ValidaDownloadByPeakyBlinders/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ValidaDownloadByPeakyBlinders {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ValidaDownloadByPeakyBlinders {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
         public static void Main() {
             ValidaDownloadByPeakyBlinders.App app = new ValidaDownloadByPeakyBlinders.App();
             app.InitializeComponent();
